10b9e8fc30fb7d93039221c0f5a3c0a56f79e555
Artur <arturmukimov716@gmail.com>
2025-05-22 14:02:15 +0500

    Add SettingsPanel in MainMenu, fix bugs

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -5,6 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject levelSelectPanel;
+    public GameObject SettingsPanel;
 
     public void Play()
     {
@@ -22,9 +23,21 @@ public class MainMenu : MonoBehaviour
         levelSelectPanel.SetActive(true);
     }
 
-    public void BackToMainMenu()
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+
+    public void BackToMainMenuFromLevelSelect()
     {
         levelSelectPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void BackToMainMenuFromSettings()
+    {
+        SettingsPanel.SetActive(false);
         mainMenuPanel.SetActive(true);
     }
 }
